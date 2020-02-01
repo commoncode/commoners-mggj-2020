4d379bd1f2acb900dcc72a88737e32243358d439
Ash Guy <ash@ashguy.com>
2020-02-01 21:01:55 +1100

    Added initial background graphics for helm and hatch

--- components/scenes/Hatch/Hatch.tsx
+++ components/scenes/Hatch/Hatch.tsx
@@ -1,7 +1,11 @@
 import { Container } from "./Hatch.styles";
+import Background from "./Background";
 
 const Hatch = ({ children, className }) => (
-  <Container className={className}>{children}</Container>
+  <Container className={className}>
+    <Background />
+    {children}
+  </Container>
 );
 
 export default Hatch;
