dc97cc38bfcaa9ef9eddad9e07bd358c75d1bafd
Esteban Campos <campos.esteban@gmail.com>
2020-02-02 10:40:27 +1100

    Added leak

--- components/events/Leak/Leak.tsx
+++ components/events/Leak/Leak.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from 'react';
 import Event from '../Event';
 import ActionCaption from '../../captions/ActionCaption';
 import useActionCaption from "../../captions/ActionCaption/useActionCaption";
+import { Leak1 } from './LeakStyles';
 
 type LeakProps = {
     x: number;
@@ -12,6 +13,7 @@ type LeakProps = {
     yell?: () => void;
     panic?: () => void;
 }
+
 
 const Leak = ({ x, y, activation, repair, yell, panic }: LeakProps) => {
     const option = useActionCaption();
@@ -45,11 +47,12 @@ const Leak = ({ x, y, activation, repair, yell, panic }: LeakProps) => {
                 x={x}
                 y={y}
                 ref={eventRef}
-                activation={(event) => {
+                activation={() => {
                     option.setShow(!option.show);
                     activation();
                 }}
             />
+            <Leak1 x={x - 50} y={y + 25} />
             <ActionCaption x={x + 20} y={y - 20} activation={() => { repair(); console.log('click repair') }} isToggled={option.show}>Repair</ActionCaption>
             <ActionCaption x={x + 20} y={y + 80} activation={() => { yell && yell(); console.log('click yell') }} isToggled={option.show}>Yell</ActionCaption>
             <ActionCaption x={x - 50} y={y + 40} activation={() => { panic && panic(); console.log('click PANIC') }} isToggled={option.show}>PANIC!</ActionCaption>
